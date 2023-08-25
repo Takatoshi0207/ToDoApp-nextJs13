dd0998821ddd87d0f84763769898549e63549604
Takatoshi0207 <t.kotani@m-okuchu.com>
2023-08-25 21:34:04 +0900

    css tweak

--- todoapp-nextjs13/src/app/page.tsx
+++ todoapp-nextjs13/src/app/page.tsx
@@ -3,10 +3,10 @@ import TodoList from './components/TodoList'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50 ">
-      <h1 >Next 13 Todo App</h1>
-      <div>
-        <div>
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200 ">
+      <h1 className='text-4xl font-bold text-gray-700 -mt-32' >Next 13 Todo App</h1>
+      <div className='w-full max-w-xl mt-5'>
+        <div className='w-full px-8 py-6 bg-white shadow-md rounded-1g'>
           <AddTask />
           <TodoList />
         </div>
