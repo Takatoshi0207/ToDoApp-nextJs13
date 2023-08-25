a551c23599baa100baebb9a53744d29d04efbadb
Takatoshi0207 <t.kotani@m-okuchu.com>
2023-08-25 22:31:30 +0900

    TodoListのcss設定
    HtmlとCssの設定完了

--- todoapp-nextjs13/src/app/components/TodoList.tsx
+++ todoapp-nextjs13/src/app/components/TodoList.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <ul className="space-y-3">
+      <li className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
+        <span>散歩</span>
+        <div>
+          <button className='text-green-500 mr-3 font-semibold'>edit</button>
+          <button className='text-red-500 font-semibold'>Delete</button>
+        </div>
+      </li>
+    </ul>
   )
 }
 
