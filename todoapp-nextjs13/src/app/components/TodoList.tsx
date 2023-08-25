9a5c3479b33d5eb77133877cef9d8aed826a91cb
Takatoshi0207 <t.kotani@m-okuchu.com>
2023-08-25 23:57:46 +0900

    idとtitileをjsonファイルから取得

--- todoapp-nextjs13/src/app/components/TodoList.tsx
+++ todoapp-nextjs13/src/app/components/TodoList.tsx
@@ -8,13 +8,17 @@ interface TodoListProps {
 const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className="space-y-3">
-      <li className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
-        <span>散歩</span>
-        <div>
-          <button className='text-green-500 mr-3 font-semibold'>edit</button>
-          <button className='text-red-500 font-semibold'>Delete</button>
-        </div>
-      </li>
+      {todos.map((todo) =>
+        <li
+          key={todo.id}
+          className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
+          <span>{todo.title}</span>
+          <div>
+            <button className='text-green-500 mr-3 font-semibold'>edit</button>
+            <button className='text-red-500 font-semibold'>Delete</button>
+          </div>
+        </li>
+      )}
     </ul>
   )
 }
