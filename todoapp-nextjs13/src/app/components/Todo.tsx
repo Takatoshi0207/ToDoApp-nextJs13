f7a3ea8973e5f79ed50253060949435705044f22
Takatoshi0207 <t.kotani@m-okuchu.com>
2023-08-26 12:12:33 +0900

    editボタンの編集
    edit押したら、isEditing状態になる。
    saveをおしたらsetIsEditing(false)になる。

--- todoapp-nextjs13/src/app/components/Todo.tsx
+++ todoapp-nextjs13/src/app/components/Todo.tsx
@@ -1,18 +1,47 @@
+'use client';
+
 import { Task } from '@/types';
-import React from 'react'
+import React, { useState } from 'react'
 
 interface TodoProps {
   todo: Task;
 }
 
 function Todo({ todo }: TodoProps) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = async () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    setIsEditing(false);
+  };
+
   return (
     <li
       key={todo.id}
       className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'>
-      <span>{todo.title}</span>
+
+      {isEditing ? (
+        <input
+          type="text"
+          className='border-gray-400 border rounded mr-2 py-1 px-2' />
+      ) : (
+        <span>{todo.title}</span>
+      )}
       <div>
-        <button className='text-green-500 mr-3 font-semibold'>edit</button>
+        {isEditing ? (
+          <button className='text-blue-500 mr-3 font-semibold'
+            onClick={handleSave}>
+            save
+          </button>
+        ) : (
+          <button className='text-green-500 mr-3 font-semibold'
+            onClick={handleEdit}>
+            edit
+          </button>
+        )}
         <button className='text-red-500 font-semibold'>Delete</button>
       </div>
     </li>
