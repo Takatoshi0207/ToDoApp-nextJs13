8c72a926bc25abe097e1d3af4273a2a06ac1677c
Takatoshi0207 <t.kotani@m-okuchu.com>
2023-08-26 11:39:32 +0900

    編集するためのAPIを作成
    どのTaskを編集するかは${id}で管理する

--- todoapp-nextjs13/src/api.ts
+++ todoapp-nextjs13/src/api.ts
@@ -1,4 +1,5 @@
 // apiを全て取得する関数を作成
+import { title } from "process";
 import { Task } from "./types";
 
 export const getAllTodos = async (): Promise<Task[]> => {
@@ -19,4 +20,16 @@ export const addTodo = async (todo: Task): Promise<Task> => {
   const newTodo = res.json();
 
   return newTodo;
+};
+
+// 編集するための関数を作成
+export const aditTodo = async (id: string, newText: string): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/todos/${id}`, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ title: newText }),
+  });
+  const updatedTodo = res.json();
+
+  return updatedTodo;
 };
